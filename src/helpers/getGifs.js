980c07338ca1300eded16a1434713f1aaedfcfeb
Irving Sanchez <irvinhirvi@gmail.com>
2024-03-22 14:05:20 -0600

    updated routes

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async (term) => {
     const api = 'sTbpdEXZ6XNtIRvpCXO41eYgNa45vaNj'
-    const url = `https://api.giphy.com/v1/gifs/search/?api_key=${api}&q=${term}&limit=10`;
+    const url = `https://api.giphy.com/v1/gifs/search/?api_key=${api}&q=${term}&limit=12`;
 
     const response = await fetch(url);
     const { data } = await response.json();
@@ -10,6 +10,5 @@ export const getGifs = async (term) => {
         title: gif.title,
         src: gif.images.downsized_medium
     }))
-    console.log(newData);
     return newData;
 }
